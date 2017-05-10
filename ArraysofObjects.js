e3e01c4c5aa8a7995ace6b8980f7302c366b79e0
patricewhite <patricewhite2012@gmail.com>
2017-05-10 16:57:28 -0400

    ArraysofObjects

---
+++ ArraysofObjects.js
@@ -0,0 +1,23 @@
+let green = [
+
+{
+  name : "Sarah",
+  job_title : "nurse"
+},
+{
+  name : "Maurice",
+  job_title : "Developer"
+},
+{
+  name : "Maya",
+  job_title : "dancer"
+},
+{
+  name : "Brian",
+  job_title : "singer"
+}
+
+]
+ for(key in green){
+   console.log(`${green[key].name} is a ${green[key].job_title}`);
+ }
